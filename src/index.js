bf3bd8929884c0e2c62de519823f83132def51bd
DeepakSilaych <deepaksilaych@gmail.com>
2024-09-08 23:42:53 +0530

    goooo

--- src/index.js
+++ src/index.js
@@ -3,14 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { HashRouter } from 'react-router-dom/dist';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
+    <App />
   </React.StrictMode>
 );
 
--- src/pages/Project/ProjectReport.jsx
+++ src/pages/Project/ProjectReport.jsx
@@ -16,15 +16,25 @@ function ProjectReport() {
                 console.log(error)
             })
     }, [id])
+
+    if (!project) {
+        return <div>Loading...</div>
+    }
     return (
         <div className='w-full flex flex-col items-center gap-4 text-white'>
+        { project.img &&
             <img
                 src={baseUrl + project.img.split('/').slice(-1)[0]}
-                alt={project.name}
-                className='w-full h-28'
+                alt='project'
+                className='w-full h-auto'
             />
+        }
             <h1 className='text-2xl font-bold'>{project.title}</h1>
-            <div className=''><a className='px-2 border-[.1em] border-[rgba(255,255,255,0.2)] rounded-lg' href={project.link}>Project Link</a></div>
+            <div className=''>
+                <a className='px-2 border-[.1em] border-[rgba(255,255,255,0.2)] rounded-lg hover:bg-[rgba(255,255,255,0.1)] hover:border-[rgba(255,255,255,0.5)]' href={project.link}>
+                    Project Link
+                </a>
+            </div>
             <pre className='whitespace-pre-wrap'>
                 {project.description}
             </pre>
